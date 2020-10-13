c5e661f5486b5a8487344457443939958bdd5b9f
Helton Isac <helton.isac@gmail.com>
2020-10-13 20:24:44 -0300

    Starting Scroll View Implementation

--- ViewCodeApp/Welcome/VIew/WelcomeView.swift
+++ ViewCodeApp/Welcome/VIew/WelcomeView.swift
@@ -17,28 +17,58 @@ protocol WelcomeViewDelegate: AnyObject {
 
 class WelcomeView: UIView, CodeView {
     
+    // MARK: - Properties
     weak var delegate: WelcomeViewDelegate?
     
-    init(delegate: WelcomeViewDelegate) {
-        super.init(frame: .zero)
-        setup()
-    }
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: .zero)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.keyboardDismissMode = .interactive
+        return scrollView
+    }()
     
+    let contentView: UIView = {
+        let contentView = UIView(frame: .zero)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
+    }()
+    
+    // MARK: - Super Methods
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
     
+    // MARK: - Methods
+    init(delegate: WelcomeViewDelegate) {
+        super.init(frame: .zero)
+        setup()
+    }
     
     func setupComponents() {
-            
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
     }
     
     func setupConstraints() {
         
+        // Layout Anchors
+        scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        scrollView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        // ContentView
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        let contentViewHeightConstraint = contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
+        contentViewHeightConstraint.priority = .defaultLow
+        contentViewHeightConstraint.isActive = true
     }
     
     func setupExtraConfigurations() {
-        backgroundColor = .white
+        backgroundColor = .blue
     }
 }
